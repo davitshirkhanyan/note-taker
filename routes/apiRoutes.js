b28919a1e6b6a5f5e62653568285627377fee22f
Davit Shirkhanyan <davit.shirkhanyan@gmail.com>
2021-02-27 22:02:38 -0800

    add post to api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -14,4 +14,24 @@ module.exports = app => {
         } 
         return res.send(404);
     });
+
+    app.post('/api/notes', (req, res) => {
+        const newNotes = req.body;
+
+        if (notes.length === 0) {
+            newNotes.id = 1;
+        } else {
+            newNotes.id = (notes[notes.length - 1].id + 1);
+        }
+        notes.push(newNotes);
+
+        let jsonNotes = JSON.stringify(notes);
+        fs.writeFile('../Develop/db/db.json', jsonNotes, err => {
+            if (err) {
+                return err;
+            }
+            console.log('Success!');
+        });
+        res.json(true);
+    });
 };
